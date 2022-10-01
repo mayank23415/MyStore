a94c877c4d8a4144476f1b3f33b8eabcbe7a027c
Himanshu Tiwari <htiwari292001@gmail.com>
2022-10-01 17:10:25 +0530

    created register activity

--- app/src/main/java/com/mystore/activities/RegisterActivity.kt
+++ app/src/main/java/com/mystore/activities/RegisterActivity.kt
@@ -6,15 +6,14 @@ import android.os.Bundle
 import android.widget.TextView
 import com.mystore.R
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
 
-        val tvRegister = findViewById<TextView>(R.id.tv_register)
-        tvRegister.setOnClickListener {
-            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+        val tvLogin = findViewById<TextView>(R.id.tv_login)
+        tvLogin.setOnClickListener {
+            startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
         }
     }
-
 }

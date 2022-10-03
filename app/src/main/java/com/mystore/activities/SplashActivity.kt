04f45b37f4b1e76c0ca6bd7a421000c6bfb86733
Himanshu Tiwari <htiwari292001@gmail.com>
2022-10-03 10:43:26 +0530

    created register activity

--- app/src/main/java/com/mystore/activities/SplashActivity.kt
+++ app/src/main/java/com/mystore/activities/SplashActivity.kt
@@ -1,16 +1,31 @@
 package com.mystore.activities
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.Window
 import android.view.WindowManager
+import androidx.core.view.WindowCompat
 import com.mystore.R
 
 @Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        //for fullscreen
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            window.attributes.layoutInDisplayCutoutMode =
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        }
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
